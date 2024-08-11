a1f7a841ec9bef326fefc6cbdcbf860e1d5df61c
Wali Muhammad <77934276+walimuhammadmalik@users.noreply.github.com>
2024-08-11 23:42:51 +0500

    Update passports.js

--- middleware/passports.js
+++ middleware/passports.js
@@ -12,43 +12,12 @@ const opts = {
   
 };
 
-// const opts = {
-//   jwtFromRequest: ExtractJwt.fromExtractors([
-//     ExtractJwt.fromAuthHeaderAsBearerToken(),
-//     ExtractJwt.fromUrlQueryParameter('token')
-//   ]),
-//   secretOrKey: process.env.JWT_SECRET, // Make sure to set this environment variable
-// };
-
-// Custom extractor to get the token from the URL query parameter 'token'
-// const extractJwtFromUrl = (req) => {
-//   let token = null;
-//   if (req && req.query && req.query.token) {
-//     token = "Bearer " + req.params;
-//   }
-//   return token;
-// };
-// console.log("extractJwtFromUrl: ", extractJwtFromUrl);
-
-// const opts = {
-//   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken() || extractJwtFromUrl,
-//   secretOrKey: process.env.JWT_SECRET, // Make sure to set this environment variable
-// };
-
 // console.log("opts: ", opts);
 console.log("passport.js");
 // create a new instance of JwtStrategy but i want to check email in both user and fbUsers
 passport.use(
   new JwtStrategy(opts, async (jwt_payload, done) => {
     // console.log("Jwt strategy: ", jwt_payload);
-    // const existingUser =
-    //   (await db.user.findOne({
-    //     where: { email: jwt_payload.email, id: jwt_payload.id },
-    //   })) ||
-    //   (await db.fbUser.findOne({
-    //     where: { email: jwt_payload.email, facebookId: jwt_payload.id },
-    //   })); // Adjust the model name as necessary
-
     const existingUser = await db.user.findOne({
       where: { email: jwt_payload.email },
     });
